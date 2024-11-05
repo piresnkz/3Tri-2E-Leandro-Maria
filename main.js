8df5eae30621e67ee393b1d96223493d1679f46d
piresnkz <160138750+piresnkz@users.noreply.github.com>
2024-11-05 08:53:58 -0300

    05/11

--- main.js
+++ main.js
@@ -1,5 +1,23 @@
 function criaCartao(categoria,pergunta,resposta){
-    console.log(categoria,pergunta,resposta)
+    let container = document.getElementById('container')
+    let article = document.createElement('article')
+    article.className = 'cartao'
+    article.innerHTML= `
+
+    <div class= 'cartao-conteudo'>
+      <h3> ${categoria} </h3>
+    <div class= 'cartao-conteudo-pergunta'>
+      <p> ${pergunta} </p>
+    </div>
+    <div class= 'cartao-conteudo-resposta'>
+      <p> ${resposta} </p>
+      </div>
+    </div>
+
+    `
+
+    container .appendChild(article)
 }
 
-criaCartao("Mateamatica","quanto é 2+2","4")
+criaCartao("Biologia", "Qual a função da mitocondria", "Produção de ATP")
+criaCartao("Quimica", "Qual significado de H20", "é um líquido formado por dois átomos de hidrogênio e um de oxigênio (Agua)")
